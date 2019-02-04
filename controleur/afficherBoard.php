944e313e86950feecd2bfe7559e859cb50e7a11d
meryemAMD <47335194+meryemAMD@users.noreply.github.com>
2019-02-04 23:05:11 +0000

    Update afficherBoard.php

--- controleur/afficherBoard.php
+++ controleur/afficherBoard.php
@@ -1,15 +1,16 @@
 <?php
 	require_once("affichageClass.php");
 	$affich = new affichage();
-	if($_SESSION["finish"]==false)
+	if($_SESSION["finish"]==false)//si la partie n'est pas terminee
 	{
-		$affich->affiche_plateau();
+		$affich->affiche_plateau(); //on affiche le plateau
 	}
 	else{
-	$affich->print_board_final();
-	unset($_SESSION["board"]); 
-	$nomGagnant = (($_SESSION["turn"] == 1)? $_SESSION["nomj1"] : $_SESSION["nomj2"]);
+	$affich->print_board_final();//si la partie est termiée on affiche l'etat finale
+	unset($_SESSION["board"]); //on supprime le platau de la session
+	$nomGagnant = (($_SESSION["turn"] == 1)? $_SESSION["nomj1"] : $_SESSION["nomj2"]);//le gagnant
 	}
+	//on modifie les variables de session
 	$turnNom = (($_SESSION["turn"] == 1)? $_SESSION["nomj1"] : $_SESSION["nomj2"]);
 	$turnPion = $_SESSION["turnPion"];
 	$j1 = $_SESSION["nomj1"];
